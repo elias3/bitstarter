8f6aa8c6c5cb66d5528028cb56f614cf93abc046
Elias Khsheibun <elias3@gmail.com>
2013-07-13 15:04:12 +0000

    reading from a local file added to web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 var stringBuffer;
 var str;
 fs.readFileSync('index.html', function (err, data) {
